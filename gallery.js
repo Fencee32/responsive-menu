eb5877e47d4386dee32eeeb6fde9219a45720504
asd <aasd@aasd.asd>
2024-04-25 08:40:22 +0200

    asd

--- gallery.js
+++ gallery.js
@@ -5,3 +5,18 @@ let Valami = ["alma", "körte", "kutya", "macska"]
 let div = document.createElement("div")
 div.innerHTML = "Hello World"
 document.body.appendChild(div)
+
+for (let i = 0; i < Valami.length; i++) {
+  let div = document.createElement("div")
+  div.innerHTML = Valami[i]
+  document.body.appendChild(div)
+}
+
+const kepek = ["delfin.png", "kutya.webp", "macska.jpg"]
+
+for (const kep of kepek) {
+  let img = document.createElement("img")
+  img.src = "img/" + kep
+  img.width = "200"
+  document.body.appendChild(img)
+}
