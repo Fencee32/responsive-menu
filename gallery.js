25060750b899007ca24d1dc51bf5d2db2b830c8e
asd <aasd@aasd.asd>
2024-05-02 08:36:11 +0200

    asd

--- gallery.js
+++ gallery.js
@@ -1,29 +1,15 @@
-console.log("H W")
-
-let Valami = ["alma", "körte", "kutya", "macska"]
-
-let div = document.createElement("div")
-div.innerHTML = "Hello World"
-document.body.appendChild(div)
-
-for (let i = 0; i < Valami.length; i++) {
-  let div = document.createElement("div")
-  div.innerHTML = Valami[i]
-  document.body.appendChild(div)
-}
+const container = document.querySelector("#container")
+const images = document.querySelector("#targetImg")
+console.log(container)
 
 const kepek = ["delfin.png", "kutya.webp", "macska.jpg"]
-const container = document.querySelector("#container")
-const targetImage = document.querySelector("#targetImage")
 
 for (const kep of kepek) {
   const img = document.createElement("img")
   img.src = "img/" + kep
   img.width = 200
-  console.log("kép:", img.src)
   container.appendChild(img)
   img.addEventListener("click", function () {
-    console.log(this.src)
-    targetImage.src = this.src
+    images.src = img.src
   })
 }
